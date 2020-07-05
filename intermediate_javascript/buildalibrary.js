b99b742b30a4247d93b4ad593dd18f3637c8ee18
benjifricker <benjifricker@icloud.com>
2020-07-05 13:54:10 -0700

    Finish build a library project

---
+++ intermediate_javascript/buildalibrary.js
@@ -0,0 +1,99 @@
+class Media {
+    constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+  
+    get title() {
+      return this._title;
+    }
+  
+    get isCheckedOut() {
+      return this._isCheckedOut;
+    }
+  
+    get ratings() {
+      return this._ratings;
+    }
+  
+    toggleCheckOutStatus() {
+      this._isCheckedOut = !this._isCheckedOut;
+    }
+  
+    getAverageRating() {
+      let sum = this._ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+      let length = this._ratings.length;
+      return sum / length;
+    }
+  
+    set isCheckedOut(isCheckedOut) {
+      this._isCheckedOut = isCheckedOut;
+    }
+  
+    addRating(rating) {
+      this._ratings.push(rating);
+    }
+  }
+  
+  
+  class Book extends Media {
+    constructor(author, title, pages) {
+      super(title);
+      this._author = author;
+      this._pages = pages;
+    }
+  
+    get author() {
+      return this._author;
+    }
+  
+    get pages() {
+      return this._pages;
+    }
+  }
+  
+  
+  class Movie extends Media {
+    constructor(director, title, runTime) {
+      super(title);
+      this._director = director;
+      this._runTime = runTime;
+    }
+  
+    get director() {
+      return this._director;
+    }
+  
+    get runTime() {
+      return this._runTime;
+    }
+  }
+  
+  
+  // Instantiate a book
+  const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+  
+  // toggleCheckOutStatus
+  historyOfEverything.toggleCheckOutStatus();
+  console.log(historyOfEverything.isCheckedOut);
+  
+  // Add ratings and log average
+  historyOfEverything.addRating(4);
+  historyOfEverything.addRating(5);
+  historyOfEverything.addRating(5);
+  console.log(historyOfEverything.getAverageRating());
+  
+  // Instantiate a movie
+  const speed = new Movie('Jan de Bont', 'Speed', 116);
+  
+  // toggleCheckOutStatus
+  speed.toggleCheckOutStatus();
+  console.log(speed.isCheckedOut);
+  
+  // Add ratings and log average
+  speed.addRating(1);
+  speed.addRating(1);
+  speed.addRating(5);
+  console.log(speed.getAverageRating());
+  
