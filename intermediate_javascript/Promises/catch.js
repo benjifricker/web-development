b64b52fa0d51aa21107cb8d33cb8861633bf025e
benjifricker <benjifricker@icloud.com>
2020-07-06 15:19:51 -0700

    Using catch() with Promises exercise

---
+++ intermediate_javascript/Promises/catch.js
@@ -0,0 +1,13 @@
+const { checkInventory } = require('./inventory.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+
+const handleFailure = (rejectedValue) => {
+  console.log(rejectedValue);
+};
+
+checkInventory(order).then(handleSuccess).catch(handleFailure);
