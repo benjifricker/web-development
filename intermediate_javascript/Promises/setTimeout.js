8a33c0a5e67c59931e746ce0754eb2e92a4cf2b3
benjifricker <benjifricker@icloud.com>
2020-07-06 15:16:45 -0700

    The Node setTimeout() Function exercise

---
+++ intermediate_javascript/Promises/setTimeout.js
@@ -0,0 +1,9 @@
+console.log("This is the first line of code in app.js.");
+
+const usingSTO = () => {
+  console.log('This is the function body.');
+};
+
+setTimeout(usingSTO, 2000);
+
+console.log("This is the last line of code in app.js.");
