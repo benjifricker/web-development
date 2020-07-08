83c25ea0900c82a79e06714addddc5295c240750
benjifricker <benjifricker@icloud.com>
2020-07-07 19:01:20 -0700

    Create library3.js

---
+++ intermediate_javascript/Promises/library3.js
@@ -0,0 +1,22 @@
+const checkAvailability = (itemName, distributorName) => {
+  console.log(`Checking availability of ${itemName} at ${distributorName}...`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (restockSuccess()) {
+            console.log(`${itemName} are in stock at ${distributorName}`)
+            resolve(itemName);
+        } else {
+            reject(`Error: ${itemName} is unavailable from ${distributorName} at this time.`);
+        }
+    }, 1000);
+  });
+};
+
+
+module.exports = { checkAvailability };
+
+
+// Set availability to True with 80% chance
+function restockSuccess() {
+  return (Math.random() > .2);
+}
