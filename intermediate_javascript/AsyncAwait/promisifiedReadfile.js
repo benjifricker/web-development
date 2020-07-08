078d5e857275284dbedbdbdf3a8a43a9eb8e4846
benjifricker <benjifricker@icloud.com>
2020-07-08 16:22:00 -0700

    Fix reject() statement

--- intermediate_javascript/AsyncAwait/promisifiedReadfile.js
+++ intermediate_javascript/AsyncAwait/promisifiedReadfile.js
@@ -4,7 +4,7 @@ const promisifiedReadfile = (file, encoding) =>
   new Promise((resolve, reject) => {
     fs.readFile(file, encoding, (err, text) => {
 			if (err) {
-				return reject(err.message);
+				reject(err.message);
       }
         resolve(text);
       });
