a6fca707017d50ba9e3ab9cf776c86f027fad13a
benjifricker <benjifricker@icloud.com>
2020-07-09 08:00:20 -0700

    Finish Handling Dependent Promises exercise

---
+++ intermediate_javascript/AsyncAwait/handlingDependentPromises.js
@@ -0,0 +1,12 @@
+const { shopForBeans, soakTheBeans, cookTheBeans } = require('./library3.js');
+
+
+async function makeBeans() {
+  let type = await shopForBeans();
+  let isSoft = await soakTheBeans(type);
+  let dinner = await cookTheBeans(isSoft);
+
+  console.log(dinner);
+}
+
+makeBeans();
