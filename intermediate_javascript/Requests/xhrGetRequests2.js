ab900158af0e5ef693e805105e9fa27c433dc715
benjifricker <benjifricker@icloud.com>
2020-07-09 22:42:01 -0700

    Refactor to import helper functions

--- intermediate_javascript/Requests/xhrGetRequests2.js
+++ intermediate_javascript/Requests/xhrGetRequests2.js
@@ -1,3 +1,5 @@
+import { renderResponse } from './helperFunctions';
+
 // API Information
 const url = 'https://api.datamuse.com/words?';
 const queryParams = 'rel_rhy=';
@@ -7,28 +9,6 @@ const queryParams = 'rel_rhy=';
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
-
-
-// Helperfunction to format response on webpage
-const renderResponse = (res) => {
-  if (!res) {
-    console.log(res.status);
-  }
-  if (!res.length) {
-    responseField.innerHTML = "<p>Try again!</p><p>There were no suggestions found!</p>";
-    return;
-  }
-
-  let wordList = [];
-
-  for (let i = 0; i < Math.min(res.length, 10); i++) {
-    wordList.push(`<li>${res[i].word}</li>`);
-  }
-  wordList = wordList.join("");
-
-  responseField.innerHTML = `<p>You might be interested in:</p><ol>${wordList}</ol>`;
-  return
-}
 
 
 // AJAX function
