f0186de11c14f76c9e7813c6ecbf9582596559a5
benjifricker <benjifricker@icloud.com>
2020-07-13 14:14:29 -0700

    Finish Business component

--- ravenous_project/ravenous/src/components/Business/Business.js
+++ ravenous_project/ravenous/src/components/Business/Business.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Business.css';
 
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
@@ -34,3 +35,5 @@ class Business extends React.Component {
     </div>
   }
 }
+
+export default Business;
