c963ff52e9677e8655c2962e215e38283d9a74f3
benjifricker <benjifricker@icloud.com>
2020-07-17 15:44:42 -0700

    Bind this for handleTermChange and handleLocationChange

--- ravenous_project/ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous_project/ravenous/src/components/SearchBar/SearchBar.js
@@ -9,6 +9,10 @@ class SearchBar extends React.Component {
       location: '',
       sortBy: 'best_match'
     };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+
     this.sortByOptions = {
       'Best Match': 'best_match',
       'Highest Rated': 'rating',
