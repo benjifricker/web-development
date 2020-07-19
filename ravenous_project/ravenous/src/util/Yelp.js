e558d0000561c085379e79a764852ad253d961d9
benjifricker <benjifricker@icloud.com>
2020-07-18 21:56:57 -0700

    Implement Yelp API call

--- ravenous_project/ravenous/src/util/Yelp.js
+++ ravenous_project/ravenous/src/util/Yelp.js
@@ -1,3 +1,30 @@
 const apiKey = 'LJIk6KG7kChVwceD5yICirJN1jLxqHGirkLQDAmQirXxhpNN-qnCCttpFRSvC3Mm83C2cfdIOicVLUV0s3-36vwpd4CHWSq7LwA9wW4Di4SHBaIYFYqz4GsGos0TX3Yx'
 
-let Yelp = {}
+let Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          return {
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }
+        });
+      }
+    });
+  }
+}
