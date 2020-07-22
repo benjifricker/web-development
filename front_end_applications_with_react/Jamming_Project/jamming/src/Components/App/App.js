abde8ae973e23cd30b0ae31f31029a5ac449248f
benjifricker <benjifricker@icloud.com>
2020-07-22 14:00:00 -0700

    Fix missing render() method

--- front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
+++ front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
@@ -12,19 +12,20 @@ class App extends React.Component {
       searchResults: []
     }
   }
-
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <Playlist />
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} />
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
