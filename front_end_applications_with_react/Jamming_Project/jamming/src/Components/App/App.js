e76cc75d92e3deb95edf4cf332460901b551ddc8
benjifricker <benjifricker@icloud.com>
2020-07-22 13:55:52 -0700

    Modify App to be a class component and add constructor

--- front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
+++ front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
@@ -5,7 +5,11 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
