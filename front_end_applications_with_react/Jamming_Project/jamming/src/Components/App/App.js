29d5a51fa7db3353b3e76b39044d09642217cefa
benjifricker <benjifricker@icloud.com>
2020-07-22 13:48:17 -0700

    Remove logo import statement

--- front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
+++ front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import { SearchBar } from '../SearchBar/SearchBar';
-import { SearchResults } from '../SearchResults/SearchResults';
-import { Playlist } from '../Playlist/Playlist';
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
 
 function App() {
   return (
