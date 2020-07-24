fd64e38d1d4ab4806159e6213093233c62bc0292
benjifricker <benjifricker@icloud.com>
2020-07-24 13:29:00 -0700

    Create getAccessToken() method for getting token and clearing after expiration time

--- front_end_applications_with_react/Jamming_Project/jamming/src/util/Spotify.js
+++ front_end_applications_with_react/Jamming_Project/jamming/src/util/Spotify.js
@@ -5,8 +5,17 @@ let Spotify = {
     if (accessToken) {
       return accessToken;
     }
-    let accessToken = window.location.href.match(/access_token=([^&]*)/);
-    let expirationTime = window.location.href.match(/expires_in=([^&]*)/);
+    let accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    let expirationTimeMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessToken && expirationTime) {
+      accessToken = accessTokenMatch[1];
+      let expiresIn = Number(expirationTimeMatch[1]);
+      // Clear accessToken
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+    }
+    return accessToken;
   }
 };
 
