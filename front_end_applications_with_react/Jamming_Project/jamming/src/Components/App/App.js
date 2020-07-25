50febc936815f52ceaad5ef4d8848aedc1faaedb
benjifricker <benjifricker@icloud.com>
2020-07-25 16:47:29 -0700

    Import Spotify module

--- front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
+++ front_end_applications_with_react/Jamming_Project/jamming/src/Components/App/App.js
@@ -5,6 +5,8 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
+import Spotify from '../../util/Spotify';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
