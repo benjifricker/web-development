bb6446c239cd5f79497a9151064f3d2789b11497
benjifricker <benjifricker@icloud.com>
2020-07-26 00:14:57 -0700

    Bugfixes and reformatting

--- front_end_applications_with_react/Jamming_Project/jamming/src/util/Spotify.js
+++ front_end_applications_with_react/Jamming_Project/jamming/src/util/Spotify.js
@@ -53,18 +53,18 @@ let Spotify = {
     let headers = { Authorization: `Bearer ${accessToken}` };
     let userId;
 
-    return fetch('https://api.spotify.com/v1/me', { headers: headers }
-    ).then(response => { response.json()}
-    ).then(jsonResponse => {
+    return fetch('https://api.spotify.com/v1/me', { headers: headers })
+    .then(response => response.json())
+    .then(jsonResponse => {
       userId = jsonResponse.id;
-      return fetch(`/v1/users/${userId}/playlists`, {
+      return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
         headers: headers,
         method: 'POST',
         body: JSON.stringify({ name: name })
-      }).then(response => response.json()
-      ).then(jsonResponse => {
+      }).then(response => response.json())
+      .then(jsonResponse => {
         let playlistId = jsonResponse.id;
-        return fetch(`/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+        return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
           headers: headers,
           method: 'POST',
           body: JSON.stringify({ uris: trackUris })
