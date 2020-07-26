a15c0bdae4e691bbdf82a62ee8e715c4c13f22a5
benjifricker <benjifricker@icloud.com>
2020-07-25 22:31:32 -0700

    Fetch userId via Spotify API

--- front_end_applications_with_react/Jamming_Project/jamming/src/util/Spotify.js
+++ front_end_applications_with_react/Jamming_Project/jamming/src/util/Spotify.js
@@ -53,6 +53,12 @@ let Spotify = {
     let accessToken = this.getAccessToken();
     let headers = { Authorization: `Bearer ${accessToken}` };
     let userId;
+
+    return fetch('https://api.spotify.com/v1/me', { headers: headers }
+    ).then(response => { response.json()}
+    ).then(jsonResponse => {
+      userId = jsonResponse.id;
+    });
   }
 };
 
