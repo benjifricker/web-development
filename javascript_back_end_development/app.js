c4aa3046bc0d38eef46f46771ab795ff9e5fbc32
benjifricker <benjifricker@icloud.com>
2020-07-27 09:33:15 -0700

    Refactor all /expressions routes into expressions module

--- javascript_back_end_development/app.js
+++ javascript_back_end_development/app.js
@@ -14,51 +14,7 @@ seedElements(expressions, 'expressions');
 
 // Expressions Router
 const expressionsRouter = require('./expressions');
-
 app.use('/expressions', expressionsRouter);
-
-// Get specific expression
-app.get('/expressions/:id', (req, res, next) => {
-  const foundExpression = getElementById(req.params.id, expressions);
-  if (foundExpression) {
-    res.send(foundExpression);
-  } else {
-    res.status(404).send();
-  }
-});
-
-// Put specific expression
-app.put('/expressions/:id', (req, res, next) => {
-  const expressionIndex = getIndexById(req.params.id, expressions);
-  if (expressionIndex !== -1) {
-    updateElement(req.params.id, req.query, expressions);
-    res.send(expressions[expressionIndex]);
-  } else {
-    res.status(404).send();
-  }
-});
-
-// Post new expression
-app.post('/expressions', (req, res, next) => {
-  const receivedExpression = createElement('expressions', req.query);
-  if (receivedExpression) {
-    expressions.push(receivedExpression);
-    res.status(201).send(receivedExpression);
-  } else {
-    res.status(400).send();
-  }
-});
-
-// Delete specific expression
-app.delete('/expressions/:id', (req, res, next) => {
-  const expressionIndex = getIndexById(req.params.id, expressions);
-  if (expressionIndex !== -1) {
-    expressions.splice(expressionIndex, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- javascript_back_end_development/expressions.js
+++ javascript_back_end_development/expressions.js
@@ -6,9 +6,52 @@ seedElements(expressions, 'expressions');
 
 const expressionsRouter = express.Router();
 
-module.exports = expressionsRouter;
-
 // Get all expressions
 expressionsRouter.get('/', (req, res, next) => {
   res.send(expressions);
 });
+
+// Get specific expression
+app.get('/:id', (req, res, next) => {
+  const foundExpression = getElementById(req.params.id, expressions);
+  if (foundExpression) {
+    res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Put specific expression
+app.put('/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    updateElement(req.params.id, req.query, expressions);
+    res.send(expressions[expressionIndex]);
+  } else {
+    res.status(404).send();
+  }
+});
+
+// Post new expression
+app.post('/', (req, res, next) => {
+  const receivedExpression = createElement('expressions', req.query);
+  if (receivedExpression) {
+    expressions.push(receivedExpression);
+    res.status(201).send(receivedExpression);
+  } else {
+    res.status(400).send();
+  }
+});
+
+// Delete specific expression
+app.delete('/:id', (req, res, next) => {
+  const expressionIndex = getIndexById(req.params.id, expressions);
+  if (expressionIndex !== -1) {
+    expressions.splice(expressionIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
+module.exports = expressionsRouter;
