2d0da3f3ec7e7a45ecbfd08dbf28aafbe654c6e3
benjifricker <benjifricker@icloud.com>
2020-07-27 10:37:12 -0700

    Add DELETE method for /puddings/:flavor route

---
+++ javascript_back_end_development/codeChallenge6.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+const puddingFlavors = ['chocolate', 'banana', 'butterscotch', 'pistachio'];
+
+const findPuddingIndex = (name) => {
+  return puddingFlavors.indexOf(name);
+}
+
+const deletePuddingAtIndex = (index) => {
+  puddingFlavors.splice(index, 1);
+}
+
+app.delete('/puddings/:flavor', (req, res, next) => {
+  const flavor = req.params.flavor;
+  const flavorIndex = findPuddingIndex(flavor)
+  if (flavorIndex != -1) {
+    deletePuddingAtIndex(flavorIndex);
+    res.status(204).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
