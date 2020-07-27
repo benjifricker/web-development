d6a40e178de184541e412442bcc73337756850fd
benjifricker <benjifricker@icloud.com>
2020-07-27 09:45:43 -0700

    Create GET method for /sausages route

--- javascript_back_end_development/codeChallenge.js
+++ javascript_back_end_development/codeChallenge.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const sausageTypes = ['bratwurst', 'andouille', 'chorizo', 'boudin', 'kielbasa'];
+
+app.get('/sausages', (req, res, next) => {
+  res.send(sausageTypes);
+});
 
 const PORT = process.env.PORT || 4001;
 
