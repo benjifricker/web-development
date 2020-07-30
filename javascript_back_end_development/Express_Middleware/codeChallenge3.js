825b8e2b345d586d6180869e45817a0a89acf5c5
benjifricker <benjifricker@icloud.com>
2020-07-30 02:38:07 -0700

    Refactor date into timeMiddleware()

--- javascript_back_end_development/Express_Middleware/codeChallenge3.js
+++ javascript_back_end_development/Express_Middleware/codeChallenge3.js
@@ -7,20 +7,18 @@ const database = {
   appetizers: ['oysters', 'dumplings', 'smoked almonds'],
 };
 
-// Add your code here:
-const timeMiddleware = () => {};
+const timeMiddleware = (req, res, next) => {
+  req.date = Date.now();
+};
 
 app.get('/snacks', (req, res, next) => {
-  const currentTime = Date.now();
-  res.send(`Snacks as of ${currentTime}: ${database.snacks}`);
+  res.send(`Snacks as of ${req.date}: ${database.snacks}`);
 });
 
 app.get('/sides', (req, res, next) => {
-  const currentTime = Date.now();
-  res.send(`Sides as of ${currentTime}: ${database.sides}`);
+  res.send(`Sides as of ${req.date}: ${database.sides}`);
 });
 
 app.get('/appetizers', (req, res, next) => {
-  const currentTime = Date.now();
-  res.send(`Appetizers as of ${currentTime}: ${database.appetizers}`);
+  res.send(`Appetizers as of ${req.date}: ${database.appetizers}`);
 });
