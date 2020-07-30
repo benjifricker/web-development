907616fe4670a95373a3c40a1c792e54783c6ca2
benjifricker <benjifricker@icloud.com>
2020-07-30 02:20:20 -0700

    Initial code

---
+++ javascript_back_end_development/Express_Middleware/codeChallenge2.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const moods = ['happy', 'exuberant', 'fanatical about middleware'];
+
+const moodleware = (req, res, next) => {
+  const randomMoodIndex = Math.floor(Math.random() * moods.length);
+  req.mood = moods[randomMoodIndex];
+}
+
+app.use(moodleware);
+
+app.get('/randomMood', (req, res, next) => {
+  res.send(req.mood);
+});
