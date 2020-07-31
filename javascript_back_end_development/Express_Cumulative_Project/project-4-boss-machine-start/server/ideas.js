68fe470f4bc090e10dee9d21bc2560b3141cd6da
benjifricker <benjifricker@icloud.com>
2020-07-31 09:09:58 -0700

    Import database methods from db module

--- javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/ideas.js
+++ javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/ideas.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const ideasRouter = express.Router();
 
+const {
+  getAllFromDatabase,
+  getFromDatabaseById,
+  addToDatabase,
+  updateInstanceInDatabase,
+  deleteFromDatabasebyId
+} = require('./db');
+
 module.exports = ideasRouter;
