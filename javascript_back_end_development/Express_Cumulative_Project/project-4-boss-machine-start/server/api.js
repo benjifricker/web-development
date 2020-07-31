72630ac6d10030e5e5ef44a9f35c1e37ba77bd06
benjifricker <benjifricker@icloud.com>
2020-07-31 04:05:22 -0700

    Mount minionsRouter for /minions endpoint

--- javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/api.js
+++ javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/api.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+// Import router modules
+const minionsRouter = require('./minions');
 
+// Use router modules
+apiRouter.use('/minions', minionsRouter);
 
 module.exports = apiRouter;
