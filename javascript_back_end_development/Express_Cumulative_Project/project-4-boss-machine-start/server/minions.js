f65c4e23f2d852ba822a641d96788de47b14349f
benjifricker <benjifricker@icloud.com>
2020-07-31 04:27:59 -0700

    Import methods from db module

--- javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/minions.js
+++ javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/minions.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const minionsRouter = express.Router();
 
+// Import methods from db module (data base)
+const { 
+  createMeeting, 
+  getAllFromDatabase, 
+  getFromDatabaseById, 
+  addToDatabase,
+  updateInstanceInDatabase, 
+  deleteFromDatabasebyId, 
+  deleteAllFromDatabase
+} = require('./db');
+
 module.exports = minionsRouter;
