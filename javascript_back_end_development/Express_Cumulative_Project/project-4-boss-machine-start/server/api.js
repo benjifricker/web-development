0a320f124cd675e2a2bf35a32ec53b3d94166a49
benjifricker <benjifricker@icloud.com>
2020-07-31 04:16:57 -0700

    Mount meetingsRouter for /meetings endpoint

--- javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/api.js
+++ javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/api.js
@@ -4,9 +4,11 @@ const apiRouter = express.Router();
 // Import router modules
 const minionsRouter = require('./minions');
 const ideasRouter = require('./ideas');
+const meetingsRouter = require('./meetings');
 
 // Use router modules
 apiRouter.use('/minions', minionsRouter);
 apiRouter.use('/ideas', ideasRouter);
+apiRouter.use('/meetings', meetingsRouter);
 
 module.exports = apiRouter;
