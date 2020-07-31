9664e450ae83903871ce8648f100ad4bc532253e
benjifricker <benjifricker@icloud.com>
2020-07-31 04:15:01 -0700

    Mount ideasRouter for /ideas endpoint

--- javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/api.js
+++ javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/api.js
@@ -3,8 +3,10 @@ const apiRouter = express.Router();
 
 // Import router modules
 const minionsRouter = require('./minions');
+const ideasRouter = require('./ideas');
 
 // Use router modules
 apiRouter.use('/minions', minionsRouter);
+apiRouter.use('/ideas', ideasRouter);
 
 module.exports = apiRouter;
