af8fdf93b7ee83b1fb39fcc9e4176a990845cab8
benjifricker <benjifricker@icloud.com>
2020-07-31 09:23:36 -0700

    Add comments

--- javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/ideas.js
+++ javascript_back_end_development/Express_Cumulative_Project/project-4-boss-machine-start/server/ideas.js
@@ -9,11 +9,13 @@ const {
   deleteFromDatabasebyId
 } = require('./db');
 
+// GET all ideas
 ideasRouter.get('/', (req, res, next) => {
   const response = getAllFromDatabase('ideas')
   res.send(response);
 })
 
+// POST new idea
 ideasRouter.post('/', (req, res, next) => {
   const newIdea = addToDatabase('ideas', req.body);
   res.status(201).send(newIdea);
